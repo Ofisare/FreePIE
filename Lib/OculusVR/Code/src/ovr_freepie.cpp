156b1d212078cac79fd606a5203e8249938f34ab
Steffen Hadlak <steffen.hadlak@zeiss.com>
2024-01-26 18:51:06 +0100

    Unified Oculus and OpenVR API

--- Lib/OculusVR/Code/src/ovr_freepie.cpp
+++ Lib/OculusVR/Code/src/ovr_freepie.cpp
@@ -2,146 +2,114 @@ extern "C"
 {
   #include "../include/ovr_freepie.h"
 }
-#include <openvr.h>
 
-vr::IVRSystem* m_system;
-unsigned int leftID;
-unsigned int rightID;
-unsigned int triggerID;
-unsigned int gripID;
-unsigned int stickID;
+#include <OVR_CAPI.h>
+#include <extras/OVR_Math.h>
 
-double	HmdFrameTiming;
+ovrSession HMD;
+
+
+double HmdFrameTiming;
 
 int ovr_freepie_init()
 {
-	if (vr::VR_IsHmdPresent() == false)
+	ovrResult result = ovr_Initialize(nullptr);
+	if (!OVR_SUCCESS(result))
 		return 1;
 
-	vr::EVRInitError eError = vr::VRInitError_None;
-	m_system = vr::VR_Init(&eError, vr::VRApplication_Other);
-	if (eError != vr::VRInitError_None)
-		return 1;		
+	ovrGraphicsLuid luid;
+	result = ovr_Create(&HMD, &luid);
+
+	if (!OVR_SUCCESS(result))
+		return 1;
+
+	if (!HMD)
+		return 1;
 
 	return 0;
 }
-
-
 
 int ovr_freepie_reset_orientation()
 {
-	//ovr_RecenterTrackingOrigin(HMD);
+	ovr_RecenterTrackingOrigin(HMD);
 	return 0;
 }
 
-int ovr_freepie_setPose(vr::TrackedDevicePose_t* pose, ovr_freepie_6dof *dof)
+int ovr_freepie_setPose(ovrPosef *pose, ovr_freepie_6dof *dof)
 {
-	vr::HmdMatrix34_t matrix = pose->mDeviceToAbsoluteTracking;
+	OVR::Matrix4<float> matrix = OVR::Matrix4<float>(pose->Orientation);
+	
+	dof->left[0] = matrix.M[0][0];
+	dof->left[1] = matrix.M[1][0];
+	dof->left[2] = matrix.M[2][0];
 
-	dof->left[0] = matrix.m[0][0];
-	dof->left[1] = matrix.m[1][0];
-	dof->left[2] = matrix.m[2][0];
+	dof->up[0] = matrix.M[0][1];
+	dof->up[1] = matrix.M[1][1];
+	dof->up[2] = matrix.M[2][1];
 
-	dof->up[0] = matrix.m[0][1];
-	dof->up[1] = matrix.m[1][1];
-	dof->up[2] = matrix.m[2][1];
+	dof->forward[0] = matrix.M[0][2];
+	dof->forward[1] = matrix.M[1][2];
+	dof->forward[2] = matrix.M[2][2];
 
-	dof->forward[0] = matrix.m[0][2];
-	dof->forward[1] = matrix.m[1][2];
-	dof->forward[2] = matrix.m[2][2];
-
-	dof->position[0] = matrix.m[0][3];
-	dof->position[1] = matrix.m[1][3];
-	dof->position[2] = matrix.m[2][3];
+	dof->position[0] = pose->Position.x;
+	dof->position[1] = pose->Position.y;
+	dof->position[2] = pose->Position.z;
 	return  0;
 }
 
 int ovr_freepie_read(ovr_freepie_data *output)
 {
-	if (!leftID)
+	HmdFrameTiming = ovr_GetPredictedDisplayTime(HMD, 0);
+	ovrTrackingState ts = ovr_GetTrackingState(HMD, ovr_GetTimeInSeconds(), HmdFrameTiming);
+	ovrSessionStatus sessionStatus;
+
+	ovrResult result = ovr_GetSessionStatus(HMD, &sessionStatus);
+
+	output->HmdMounted = sessionStatus.HmdMounted;
+	output->statusHead = ts.StatusFlags == (ovrStatus_OrientationTracked | ovrStatus_PositionTracked) ? 2 : ts.StatusFlags > 0 ? 1 : 0;
+	output->statusLeftHand = ts.HandStatusFlags[ovrHand_Left] == (ovrStatus_OrientationTracked | ovrStatus_PositionTracked) ? 2 : ts.HandStatusFlags[ovrHand_Left] > 0 ? 1 : 0;
+	output->statusRightHand = ts.HandStatusFlags[ovrHand_Right] == (ovrStatus_OrientationTracked | ovrStatus_PositionTracked) ? 2 : ts.HandStatusFlags[ovrHand_Right] > 0 ? 1 : 0;
+
+	ovrPosef headpose = ts.HeadPose.ThePose;
+	ovrPosef lhandPose = ts.HandPoses[ovrHand_Left].ThePose;
+	ovrPosef rhandPose = ts.HandPoses[ovrHand_Right].ThePose;
+	ovr_freepie_setPose(&headpose, &output->head);
+	ovr_freepie_setPose(&lhandPose, &output->leftHand);
+	ovr_freepie_setPose(&rhandPose, &output->rightHand);
+
+	ovrInputState inputState;
+	if (OVR_SUCCESS(ovr_GetInputState(HMD, ovrControllerType_Touch, &inputState)))
 	{
-		for (unsigned int id = 0; id < vr::k_unMaxTrackedDeviceCount; id++) {
-			vr::ETrackedDeviceClass trackedDeviceClass = m_system->GetTrackedDeviceClass(id);
-			if (trackedDeviceClass != vr::ETrackedDeviceClass::TrackedDeviceClass_Controller || !m_system->IsTrackedDeviceConnected(id))
-				continue;
+		output->A = ((inputState.Buttons & ovrButton_A) == ovrButton_A) ? 1 : ((inputState.Touches & ovrTouch_A) == ovrTouch_A) ? 0.5 : 0;
+		output->B = ((inputState.Buttons & ovrButton_B) == ovrButton_B) ? 1 : ((inputState.Touches & ovrTouch_B) == ovrTouch_B) ? 0.5 : 0;
+		output->X = ((inputState.Buttons & ovrButton_X) == ovrButton_X) ? 1 : ((inputState.Touches & ovrTouch_X) == ovrTouch_X) ? 0.5 : 0;
+		output->Y = ((inputState.Buttons & ovrButton_Y) == ovrButton_Y) ? 1 : ((inputState.Touches & ovrTouch_Y) == ovrTouch_Y) ? 0.5 : 0;
+		output->LThumb = ((inputState.Buttons & ovrButton_LThumb) == ovrButton_LThumb) ? 1 : ((inputState.Touches & ovrTouch_LThumb) == ovrTouch_LThumb) ? 0.5 : 0;
+		output->RThumb = ((inputState.Buttons & ovrButton_RThumb) == ovrButton_RThumb) ? 1 : ((inputState.Touches & ovrTouch_RThumb) == ovrTouch_RThumb) ? 0.5 : 0;
+		output->Menu = ((inputState.Buttons & ovrButton_Enter) == ovrButton_Enter) ? 1 : 0;
+		output->Home = ((inputState.Buttons & ovrButton_Home) == ovrButton_Home) ? 1 : 0;
 
-			// Confirmed that the device in question is a connected controller
-			vr::ETrackedControllerRole role = m_system->GetControllerRoleForTrackedDeviceIndex(id);
-			if (role == vr::TrackedControllerRole_LeftHand)
-			{
-				leftID = id;
-
-				for (int x = 0; x < vr::k_unControllerStateAxisCount; x++)
-				{
-					int prop = m_system->GetInt32TrackedDeviceProperty(id, (vr::ETrackedDeviceProperty)(vr::Prop_Axis0Type_Int32 + x));
-
-					if (prop == vr::k_eControllerAxis_Trigger)
-						if (triggerID == 0)
-							triggerID = x;
-						else
-							gripID = x;
-					else if (prop == vr::k_eControllerAxis_TrackPad || prop == vr::k_eControllerAxis_Joystick)
-						stickID = x;
-				}
-			}
-			else if (role == vr::TrackedControllerRole_RightHand)
-			{
-				rightID = id;
-			}
-		}
+		output->LTrigger = inputState.IndexTrigger[ovrHand_Left];
+		output->LGrip = inputState.HandTrigger[ovrHand_Left];
+		output->Lstick[0] = inputState.Thumbstick[ovrHand_Left].x;
+		output->Lstick[1] = inputState.Thumbstick[ovrHand_Left].y;
+		output->RTrigger = inputState.IndexTrigger[ovrHand_Right];
+		output->RGrip = inputState.HandTrigger[ovrHand_Right];
+		output->Rstick[0] = inputState.Thumbstick[ovrHand_Right].x;
+		output->Rstick[1] = inputState.Thumbstick[ovrHand_Right].y;
 	}
 
-	vr::TrackedDevicePose_t headTrackedDevicePose;
-
-	m_system->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseStanding, 0, &headTrackedDevicePose, 1);
-	ovr_freepie_setPose(&headTrackedDevicePose, &output->head);
-	output->statusHead = headTrackedDevicePose.eTrackingResult;
-
-	vr::TrackedDevicePose_t leftTrackedDevicePose;
-	vr::VRControllerState_t lefControllerState;
-	m_system->GetControllerStateWithPose(vr::TrackingUniverseStanding, leftID, &lefControllerState, sizeof(lefControllerState), &leftTrackedDevicePose);
-	ovr_freepie_setPose(&leftTrackedDevicePose, &output->leftHand);
-	output->statusLeftHand = leftTrackedDevicePose.eTrackingResult;
-	output->LTrigger = lefControllerState.rAxis[triggerID].x;
-	output->LGrip = lefControllerState.rAxis[gripID].x;
-	output->Lstick[0] = lefControllerState.rAxis[stickID].x;
-	output->Lstick[1] = lefControllerState.rAxis[stickID].y;
-	output->Lbuttons = (unsigned int)lefControllerState.ulButtonPressed;
-	output->Ltouches = (unsigned int)lefControllerState.ulButtonTouched;
-
-	vr::TrackedDevicePose_t rightTrackedDevicePose;
-	vr::VRControllerState_t rightControllerState;
-	m_system->GetControllerStateWithPose(vr::TrackingUniverseStanding, rightID, &rightControllerState, sizeof(rightControllerState), &rightTrackedDevicePose);
-	ovr_freepie_setPose(&rightTrackedDevicePose, &output->rightHand);
-	output->statusRightHand = rightTrackedDevicePose.eTrackingResult;
-	output->RTrigger = rightControllerState.rAxis[triggerID].x;
-	output->RGrip = rightControllerState.rAxis[gripID].x;
-	output->Rstick[0] = rightControllerState.rAxis[stickID].x;
-	output->Rstick[1] = rightControllerState.rAxis[stickID].y;
-	output->Rbuttons = (unsigned int)rightControllerState.ulButtonPressed;
-	output->Rtouches = (unsigned int)rightControllerState.ulButtonTouched;
-
-	output->HmdMounted = m_system->GetTrackedDeviceActivityLevel(0) == vr::k_EDeviceActivityLevel_UserInteraction;
-	
 	return 0;
 }
-int ovr_freepie_trigger_haptic_pulse(unsigned int controllerIndex, unsigned int axis, unsigned int durationMicroSec)
+int ovr_freepie_trigger_haptic_pulse(unsigned int controllerIndex, unsigned int durationMicroSec, float frequency, float amplitude)
 {
 	if (controllerIndex == 0)
 	{
-		if (leftID)
-		{
-			m_system->TriggerHapticPulse(leftID, axis, (unsigned short)durationMicroSec);
-			return 1;
-		}
+		ovr_SetControllerVibration(HMD, ovrControllerType_LTouch, frequency, amplitude);
 	}
 	else
 	{
-		if (rightID)
-		{
-			m_system->TriggerHapticPulse(rightID, axis, (unsigned short)durationMicroSec);
-			return 2;
-		}
+		ovr_SetControllerVibration(HMD, ovrControllerType_RTouch, frequency, amplitude);
 	}
 
 	return 0;
@@ -149,7 +117,8 @@ int ovr_freepie_trigger_haptic_pulse(unsigned int controllerIndex, unsigned int 
 
 int ovr_freepie_destroy()
 {
-	vr::VR_Shutdown();
+	ovr_Destroy(HMD);
+	ovr_Shutdown();
 
 	return 0;
 }
